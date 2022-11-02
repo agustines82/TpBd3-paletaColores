7c1bc73b4e907cedaad61ebbea241651103c75e8
Agustin Sampayo <agustines82@gmail.com>
2022-11-02 13:10:41 -0300

    validaciones

--- src/routes/colores.routes.js
+++ src/routes/colores.routes.js
@@ -2,5 +2,12 @@ import { Router } from "express";
 import { borrarColor, agregarColor, editarColor, listarColores, obtenerColor } from "../controllers/colores.controllers";
 import { check } from "express-validator";
 const router = Router();
-router.route("/colores").get(listarColores).post(agregarColor);
-router.route("/colores/:id").get(obtenerColor).put(editarColor).delete(borrarColor);
+router
+    .route("/colores")
+    .get(listarColores)
+    .post([check("nombreColor").notEmpty().withMessage("El nombre del color es un dato obligatorio")], agregarColor);
+router
+    .route("/colores/:id")
+    .get(obtenerColor)
+    .put([check("nombreColor").notEmpty().withMessage("El nombre del color es un dato obligatorio")], editarColor)
+    .delete(borrarColor);
